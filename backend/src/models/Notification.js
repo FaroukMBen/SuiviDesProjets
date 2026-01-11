5b64c9531bd2422963f176c265f62a1f68111234
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2026-01-11 21:15:43 +0100

    feat(campaign): implémentation du système d'invitation auto-join et gestion des membres

--- backend/src/models/Notification.js
+++ backend/src/models/Notification.js
@@ -5,10 +5,11 @@ const notificationSchema = new mongoose.Schema({
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     type: { type: String, enum: ['INVITATION', 'INFO'], required: true },
     project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' },
-    task: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' }, // Reference to the task related to this notification
+    campaign: { type: mongoose.Schema.Types.ObjectId, ref: 'Campagne' },
+    task: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' },
     message: { type: String },
     status: { type: String, enum: ['unread', 'read'], default: 'unread' },
-    actionStatus: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending' }, // For invitations
+    actionStatus: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending' },
     createdAt: { type: Date, default: Date.now }
 });
 

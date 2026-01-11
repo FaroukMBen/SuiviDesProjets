d9461c026417d264bded21bed25b9d5294b619b0
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2026-01-11 15:46:10 +0100

    feat(evaluations): création page globale pour visualiser les évaluations de tous les projets

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -8,37 +8,38 @@ const path = require('path');
 class ProjectController {
   // controllers/projectController.js
   static async getAllProjects(req, res) {
-      try {
-        const { campaign } = req.query; // Récupère le paramètre ?campaign=... de l'URL
-        let filter = {};
+    try {
+      const { campaign } = req.query; // Récupère le paramètre ?campaign=... de l'URL
+      let filter = {};
 
-        // 1. FILTRE PAR CAMPAGNE
-        // C'est ça qui permet d'afficher seulement les projets de la campagne X
-        if (campaign) {
-          filter.campaign = campaign;
-        }
-
-        if (req.user.role === 'student') {
-          // L'étudiant doit être owner OU member
-          filter.$or = [
-              { owner: req.user.id },
-              { members: req.user.id }
-          ];
-        }
-
-        const projects = await Project.find(filter)
-          .populate('owner', 'name') // On veut le nom du chef de projet
-          .sort({ updatedAt: -1 }); // Les plus récents en premier
-
-        res.status(200).json({ 
-            success: true, 
-            count: projects.length,  
-            projects 
-        });
-
-      } catch (err) {
-        res.status(500).json({ success: false, message: err.message });
+      // 1. FILTRE PAR CAMPAGNE
+      // C'est ça qui permet d'afficher seulement les projets de la campagne X
+      if (campaign) {
+        filter.campaign = campaign;
       }
+
+      if (req.user.role === 'student') {
+        // L'étudiant doit être owner OU member
+        filter.$or = [
+          { owner: req.user.id },
+          { members: req.user.id }
+        ];
+      }
+
+      const projects = await Project.find(filter)
+        .populate('owner', 'name') // On veut le nom du chef de projet
+        .populate('members', 'name profilePicture') // On veut les membres pour l'affichage
+        .sort({ updatedAt: -1 }); // Les plus récents en premier
+
+      res.status(200).json({
+        success: true,
+        count: projects.length,
+        projects
+      });
+
+    } catch (err) {
+      res.status(500).json({ success: false, message: err.message });
+    }
   }
 
   static async createProject(req, res) {

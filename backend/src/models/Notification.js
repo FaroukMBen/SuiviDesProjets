742bf5133ba2f05000746ab58a24dda473d5b831
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2026-01-27 17:56:39 +0100

    feat(backend): ajout et mise à jour des modèles pour la messagerie

--- backend/src/models/Notification.js
+++ backend/src/models/Notification.js
@@ -7,6 +7,7 @@ const notificationSchema = new mongoose.Schema({
     project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' },
     campaign: { type: mongoose.Schema.Types.ObjectId, ref: 'Campagne' },
     task: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' },
+    conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },
     message: { type: String },
     status: { type: String, enum: ['unread', 'read'], default: 'unread' },
     actionStatus: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending' },
